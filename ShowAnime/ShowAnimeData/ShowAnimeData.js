a68d613b6f09124fbf1966e345e4edd9f8890c3e
SuperShow420 <110090589+SuperShow420@users.noreply.github.com>
2023-01-08 19:09:14 +0530

    Update ShowAnimeData.js

--- ShowAnime/ShowAnimeData/ShowAnimeData.js
+++ ShowAnime/ShowAnimeData/ShowAnimeData.js
@@ -1,32 +1,29 @@
 const Anime = [
-        {
-        animeName : 'Chainsaw Man',
-        otherNames : ["チェンソーマン"], 
-        description : `Denji is robbed of a normal teenage life, left with nothing but his deadbeat father's overwhelming debt. His only companion is his pet, the chainsaw devil Pochita, with whom he slays devils for money that inevitably ends up in the yakuza's pockets. All Denji can do is dream of a good, simple life: one with delicious food and a beautiful girlfriend by his side. But an act of greedy betrayal by the yakuza leads to Denji's brutal, untimely death, crushing all hope of him ever achieving happiness.  Remarkably, an old contract allows Pochita to merge with the deceased Denji and bestow devil powers on him, changing him into a hybrid able to transform his body parts into chainsaws. Because Denji's new abilities pose a significant risk to society, the Public Safety Bureau's elite devil hunter Makima takes him in, letting him live as long as he obeys her command. Guided by the promise of a content life alongside an attractive woman, Denji devotes everything and fights with all his might to make his naive dreams a reality.`,
-        type : "TV",
-        episodesRuntime : "12",
-        status : "Currently Airing",
-        releaeDate : "Oct 12, 2022 to ?",
-        related : ["Chainsaw Man"],
-        genre : "Action, Supernatural",
-        studio : "MAPPA",
-        imgVertical : "https://drive.google.com/uc?export=view&id=1DyQgU_RFs50WPiGhwTNQ-jhRPU-kbwda",
-        imgVerticalHighQuality : "https://drive.google.com/uc?export=view&id=1DyQgU_RFs50WPiGhwTNQ-jhRPU-kbwda",
-        imgHorizontal : "https://drive.google.com/uc?export=view&id=1BdKWTU5GGkpUvh1PqgDR9PAl3Wf700Kp",
-        imgHorizontalHighQuality : "https://drive.google.com/uc?export=view&id=1BdKWTU5GGkpUvh1PqgDR9PAl3Wf700Kp",
-        serverSub : {
-                streamsb : {
-                        },
-                filemoon : [
-                        '<iframe src="https://filemoon.sx/e/0y17fjgsxu82/AnimePahe_Chainsaw_Man_-_01_1080p_SubsPlease" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" width="640" height="360" allowfullscreen></iframe>',
-                        '<iframe src="https://filemoon.sx/e/9gkminflvowb/AnimePahe_Chainsaw_Man_-_02_1080p_SubsPlease" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" width="640" height="360" allowfullscreen></iframe>',
-                        '<iframe src="https://filemoon.sx/e/ajfc1qu4o9qi/AnimePahe_Chainsaw_Man_-_03_1080p_SubsPlease" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" width="640" height="360" allowfullscreen></iframe>',
-                        '<iframe src="https://filemoon.sx/e/iltgvc68zeya/AnimePahe_Chainsaw_Man_-_04_1080p_SubsPlease" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" width="640" height="360" allowfullscreen></iframe>',
-                        ],
-                streamtape : {
-                        },
-                doodstream : {
-                        },
-                }
-        },
+    {
+                name : 'Tokyo Revengers: Seiya Kessen-hen',
+                otherNames : {"Tokyo Revengers: Christmas Showdown"," 東京リベンジャーズ 聖夜決戦編"}, 
+                description : 'Second season of Tokyo Revengers.',
+                type : "TV",
+                episodesRuntime : "13",
+                status : "Currently Airing",
+                releaeDate : "Jan 8, 2023 to ?",
+                related : "Tokyo Revengers",
+                genre : "Action, Drama, Supernatural",
+                studio : "LIDENFILMS",
+                imgVertical : "https://drive.google.com/uc?export=view&id=19Hrpiny_tt1rp4UDKonjNTGSQPqWu9v8",
+                imgVerticalHighQuality : "https://drive.google.com/uc?export=view&id=19Hrpiny_tt1rp4UDKonjNTGSQPqWu9v8",
+                imgHorizontal : "https://drive.google.com/uc?export=view&id=1aGVhiMeeXgnXZlvNybSOAIfQZdQpCr9u",
+                imgHorizontalHighQuality : "https://drive.google.com/uc?export=view&id=1aGVhiMeeXgnXZlvNybSOAIfQZdQpCr9u",
+                server : {
+                           streamsb : {
+                                   <IFRAME SRC="https://sbchill.com/e/3f5gl9ct5a8n.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=640 HEIGHT=360 allowfullscreen></IFRAME>
+                                    },
+                            filemoon : {
+                                    },
+                            streamtape : {
+                                    },
+                            doodstream : {
+                                    },
+                            }
+},
 ]
